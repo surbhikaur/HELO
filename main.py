7b39fed7d202941bca3d14e52a48b5895eab5853
surbhikaur <75517818+surbhikaur@users.noreply.github.com>
2020-12-09 12:01:59 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+city = "punjab"
+patiala=67
+punjab=45
+harayana=78
+patiala=65
+print("hloo i am a bus conductor")
+city=input("enter your city name")
+print(city)
+if city  ==  punjab : 
+  print("welcome you are enter in punjab" )
+  print("your chargeis",punjab)
+  print("thanks your ticket is confirmed")
+elif destination == "harayana":
+  print("your charges is",harayana)
+  print("thanks your ticket is confirmed")
+ elif destination == "patiala":
+  print("your charges is ",patiala)
+  print("thanks your ticket is confirmed")
+ elif destination == "jalandhar":
+  print("your charges is ",jalandhar)
+  print("thanks your ticket is confirmed")
+else :
+print("this bus are not going toanother station")
+
+  
